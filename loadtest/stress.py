1047817f01890dae3abe4e262c85b0f92dec4492
Ryan Kelly <rfkelly@mozilla.com>
2012-04-04 18:36:33 -0700

    Enable loadtests to use a live tokenserver.

--- loadtest/stress.py
+++ loadtest/stress.py
@@ -11,27 +11,36 @@ import random
 import json
 import time
 
+from ConfigParser import NoOptionError
 from funkload.FunkLoadTestCase import FunkLoadTestCase
 from funkload.utils import Data
 
 import macauthlib
 from webob import Request
 from mozsvc.user.whoauth import SagradaMACAuthPlugin
+from tokenserver.tests.support import get_assertion
+
 
 VERSION = '1.0'
 put_count_distribution = [0, 18, 67, 9, 4, 2]  # 0% 0 PUTs, 18% 1 PUT, etc.
-
-# Use the auth plugin to generate fake auth tokens.
-# It must be configured to use the same secrets as the server under test.
-auth_plugin = SagradaMACAuthPlugin(secret="TED KOPPEL IS A ROBOT")
 
 
 class StressTest(FunkLoadTestCase):
 
     def setUp(self):
-        nodes = self.conf_get("main", "nodes").split("\n")
-        nodes = [node.strip() for node in nodes]
-        self.nodes = [node for node in nodes if not node.startswith("#")]
+        # Should we use a tokenserver or synthesize our own?
+        try:
+            self.token_server_url = self.conf_get("main", "token_server_url")
+            self.logd("using tokenserver at %s" % (self.token_server_url,))
+        except NoOptionError:
+            self.token_server_url = None
+            secrets_file = self.conf_get("main", "secrets_file")
+            auth_plugin = SagradaMACAuthPlugin(secrets_file=secrets_file)
+            nodes = self.conf_get("main", "endpoint_nodes").split("\n")
+            nodes = [node.strip() for node in nodes]
+            nodes = [node for node in nodes if not node.startswith("#")]
+            self.endpoint_nodes = nodes
+            self.logd("using secrets_file from %s" % (secrets_file,))
 
     def setMACAuthHeader(self, method, url, token, secret):
         """Set the Sagrada MAC Auth header using the given credentials."""
@@ -42,19 +51,15 @@ class StressTest(FunkLoadTestCase):
         self.addHeader("Authorization", req.environ["HTTP_AUTHORIZATION"])
 
     def test_app_storage_session(self):
-        userid = self._pick_user()
-        node = self._pick_node()
-        self.logd("choosing node %s" % (node))
-
-        # Generate authentication token
-        req = Request.blank(node)
-        token, secret = auth_plugin.encode_mac_id(req, {"uid": userid})
+        token, secret, endpoint_url = self._generate_token()
 
         # Initial GET of the (empty) set of apps.
         self.setOkCodes([200])
-        url = node + "/%s/%s/apps/" % (VERSION, userid)
+        url = endpoint_url + "/apps/"
         self.setMACAuthHeader("GET", url, token, secret)
         response = self.get(url)
+        apps = json.loads(response.body)["apps"]
+        self.assertEquals(len(apps), 0)
 
         TEST_APP_DATA = {
             "origin": "https://example.com",
@@ -72,25 +77,18 @@ class StressTest(FunkLoadTestCase):
             data["origin"] = "https://example%d.com" % (x,)
             id = hashlib.sha1(data["origin"]).digest()
             id = base64.urlsafe_b64encode(id).rstrip("=")
-            url = node + "/%s/%s/apps/%s"
-            url = url % (VERSION, userid, id)
+            url = endpoint_url + "/apps/" + id
             data = Data('application/json', json.dumps(data))
             self.logd("about to PUT (x=%d) %s" % (x, url))
             self.setMACAuthHeader("PUT", url, token, secret)
             response = self.put(url, params=data)
 
             self.setOkCodes([200])
-            url = node + "/%s/%s/apps/" % (VERSION, userid)
+            url = endpoint_url + "/apps/"
             self.setMACAuthHeader("GET", url, token, secret)
             response = self.get(url)
-
-    def _pick_node(self):
-        """Randomly select a node on which to store the apps."""
-        return random.choice(self.nodes)
-
-    def _pick_user(self):
-        """Randomly select a userid for use during storage."""
-        return random.randint(1, 1000000)
+            apps = json.loads(response.body)["apps"]
+            self.assertEquals(len(apps), x + 1)
 
     def _pick_weighted_count(self, weights):
         target = random.randint(1, sum(weights))
@@ -100,3 +98,29 @@ class StressTest(FunkLoadTestCase):
             if total >= target:
                 return i
         assert False, "_pick_weighted_count ran off the end of the list"
+
+    def _generate_token(self):
+        """Pick an identity, log in and generate the auth token."""
+        uid = random.randint(1, 1000000)
+        # Use the tokenserver if configured, otherwise fake it ourselves.
+        if self.token_server_url is None:
+            self.logd("synthesizing token for uid %s" % (uid,))
+            endpoint_node = random.choice(self.endpoint_nodes)
+            req = Request.blank(endpoint_node)
+            token, secret = self.auth_plugin.encode_mac_id(req, {"uid": uid})
+            endpoint_url = endpoint_node + "/%s/%s/" % (VERSION, uid)
+        else:
+            email = "user_%s@loadtest.local" % (uid,)
+            self.logd("requesting token for %s" % (email,))
+            assertion = get_assertion(email, issuer="loadtest.local")
+            token_url = self.token_server_url + "/1.0/aitc/1.0"
+            self.addHeader("Authorization", "Browser-ID " + assertion)
+            response = self.get(token_url)
+            credentials = json.loads(response.body)
+            token = credentials["id"].encode("ascii")
+            secret = credentials["key"].encode("ascii")
+            endpoint_url = credentials["api_endpoint"]
+
+        self.logd("assigned endpoint_url %s" % (endpoint_url))
+        return token, secret, endpoint_url
+
