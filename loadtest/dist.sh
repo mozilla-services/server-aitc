57820cf99c341fd9119e51bef103e75f0df1ac9e
Ryan Kelly <rfkelly@mozilla.com>
2012-06-05 07:38:46 +1000

    Make the loadtest-distributing script clean up after itself when killed.

--- loadtest/dist.sh
+++ loadtest/dist.sh
@@ -4,11 +4,16 @@ workers="client4.scl2.svc.mozilla.com client5.scl2.svc.mozilla.com client6.scl2.
 dest_dir="$HOME/aitc-loadtest"
 source_dir=$(dirname $0)
 
+trap "echo ==> 'killing bench runs'; xapply 'ssh %1 killall fl-run-bench' $workers" EXIT
+
+echo "==> killing existing bench runs"
+xapply "ssh %1 killall fl-run-bench" $workers
+
 echo "==> syncing files to workers"
-xapply -xP10 "rsync $source_dir/{StressTest.conf,stress.py,Makefile} %1:$dest_dir/" $workers
+xapply "rsync $source_dir/{StressTest.conf,stress.py,Makefile,aitc-1.0.stage} %1:$dest_dir/" $workers
 
 echo "==> building virtualenvs"
-xapply -xP10 "ssh %1 cd $dest_dir \; make build" $workers
+xapply "ssh %1 cd $dest_dir \; rm loadtest*\; make build" $workers
 
 echo "==> running load"
 while :; do
