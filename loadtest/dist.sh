d2a0a95b89ce5f8309b0a1447c4e6be46855ffc7
Pete Fritchman <petef@databits.net>
2012-06-21 14:10:51 -0700

    speed up virtualenv build; add a mode to clean up old installs

--- loadtest/dist.sh
+++ loadtest/dist.sh
@@ -1,19 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
 workers="client4.scl2.svc.mozilla.com client5.scl2.svc.mozilla.com client6.scl2.svc.mozilla.com client7.scl2.svc.mozilla.com client8.scl2.svc.mozilla.com client9.scl2.svc.mozilla.com"
 dest_dir="$HOME/aitc-loadtest"
 source_dir=$(dirname $0)
 
+if [ "$1" = "clean" ]; then
+    echo "==> cleaning up existing loadtest virtualenvs"
+    xapply -xP10 "ssh %1 rm -rf $dest_dir" $workers
+fi
+
 trap "echo ==> 'killing bench runs'; xapply 'ssh %1 killall fl-run-bench' $workers" EXIT
 
 echo "==> killing existing bench runs"
-xapply -x "ssh %1 killall fl-run-bench 2\>/dev/null" $workers
+xapply -xP10 "ssh %1 killall fl-run-bench 2\>/dev/null" $workers
 
 echo "==> syncing files to workers"
-xapply -x "rsync $source_dir/{StressTest.conf,stress.py,Makefile} %1:$dest_dir/" $workers
+xapply -xP10 "rsync $source_dir/{StressTest.conf,stress.py,Makefile} %1:$dest_dir/" $workers
 
 echo "==> building virtualenvs"
-xapply -x "ssh %1 cd $dest_dir \; rm -f loadtest\* \; make build" $workers
+xapply -xP10 "ssh %1 cd $dest_dir \; rm -f loadtest\* \; make build" $workers
 
 echo "==> running load"
 while :; do
