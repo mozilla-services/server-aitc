6e410163dfb496bba4f8001308c50d5488fa3bb7
Ryan Kelly <rfkelly@mozilla.com>
2012-07-12 14:57:00 +1000

    DO NOT MERGE;  log app install/uninstall events

--- aitc/controller.py
+++ aitc/controller.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+import hashlib
 import simplejson as json
 
 from pyramid.httpexceptions import (HTTPNotFound,
@@ -10,6 +11,7 @@ from pyramid.httpexceptions import (HTTPNotFound,
                                     HTTPUnsupportedMediaType)
 
 from mozsvc.exceptions import ERROR_MALFORMED_JSON, ERROR_INVALID_OBJECT
+from mozsvc.metrics import update_mozsvc_data
 
 from syncstorage.controller import StorageController, HTTPJsonBadRequest
 
@@ -42,6 +44,7 @@ class AITCController(object):
     def get_collection(self, request):
         """Get the list of items from a collection."""
         kwds = {"full": "1"}
+        collection = request.matchdict["collection"]
         if "after" in request.GET:
             kwds["newer"] = request.GET["after"]
         try:
@@ -51,12 +54,17 @@ class AITCController(object):
         items = (json.loads(bso["payload"]) for bso in bsos)
         if "full" not in request.GET:
             items = (self._abbreviate_item(request, item) for item in items)
-        return {request.matchdict["collection"]: list(items)}
+        items = list(items)
+        for item in items:
+            self._log_record_seen(request, collection, item)
+        return {collection: items}
 
     def get_item(self, request):
         """Get a single item by ID."""
         bso = self.controller.get_item(request)
-        return json.loads(bso["payload"])
+        item = json.loads(bso["payload"])
+        self._log_record_seen(request, request.matchdict["collection"], item)
+        return item
 
     def set_item(self, request):
         """Upload a new item by ID."""
@@ -77,6 +85,7 @@ class AITCController(object):
         # trigger pyramid's prompt-for-credentials handlers.
         if request.matchdict["item"] != item.get_id():
             return HTTPForbidden("Item ID does not match origin")
+        self._log_record_seen(request, request.matchdict["collection"], item)
         # Pass through to storage.
         # Requires another round-trip through JSON; yuck.
         request.body = json.dumps({"payload": json.dumps(item)})
@@ -121,3 +130,24 @@ class AITCController(object):
         # Don't error out if the database contains items with unknown fields.
         item = RecordClass(data, ignore_unknown_fields=True)
         return item.abbreviate()
+
+    def _log_record_seen(self, request, collection, item):
+        """Log the fact that a record has been seen by a particular client.
+
+        This method is a hook for metrics logging.  It gets called each time
+        that a record is sent to or received from a client, and logs that
+        information for future reporting and analysis.
+        """
+        if collection == "apps":
+            fingerpint = hashlib.sha1()
+            fingerprint.update(request.user["uid"])
+            fingerprint.update(request.headers.get("User-Agent", ""))
+            fingerprint.update(item["origin"])
+            if item.get("hidden", False):
+                action = "uninstall"
+            else:
+                action = "install"
+            update_mozsvc_data({
+                "aitc.fingerprint": fingerprint.hexdigest(),
+                "aitc.action": action,
+            })
